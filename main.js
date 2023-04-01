fcf88c785a9cd5f6e4ce989f6a41eb6636c714dd
John Doe <johndoe@example.com>
2023-04-01 20:16:45 +0400

    register ready

---
+++ main.js
@@ -0,0 +1,4 @@
+const firstNumber = +prompt('Type the first number');
+const secondNumber = +prompt('Type the second number');
+const sumResult = ('The result is ' + (firstNumber + secondNumber));
+console.log(sumResult);
